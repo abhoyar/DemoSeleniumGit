fc9d7248e109e8fba92db3c7e5c3b76968beb030
Aakash Rewaram Bhoyar <abhoyar@evoketechnologies.com>
2019-05-20 16:43:45 +0530

    added search page

--- src/main/java/com/qa/libs/TestBase.java
+++ src/main/java/com/qa/libs/TestBase.java
@@ -5,4 +5,8 @@ public class TestBase {
 	public void test() {
 		System.out.println("heelo test base");
 	}
+	public void test(String str) {
+		System.out.println("heelo test base : " +str);
+	}
+	
 }
---
+++ src/main/java/com/qa/pages/Search.java
@@ -0,0 +1,7 @@
+package com.qa.pages;
+
+public class Search {
+	public void Searchtext() {
+		System.out.println("Searchtext page ");
+	}
+}
